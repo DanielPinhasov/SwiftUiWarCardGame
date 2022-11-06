488346ef6fd8d56492a18c94e56b34318f6f84f5
Daniel <81879122+DanielPinhasov@users.noreply.github.com>
2022-11-06 13:10:16 +0200

    Initial Commit

---
+++ SwiftUiWarCardGame/SwiftUiWarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiWarCardGameApp.swift
+//  SwiftUiWarCardGame
+//
+//  Created by דניאל פינחסוב on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiWarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
